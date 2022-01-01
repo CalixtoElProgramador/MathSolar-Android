ca2432a1c9e7ff9b4733e08edc44ec6f6d460059
CalixtoElProgramador <calixto.programador@gmail.com>
2021-12-31 19:40:21 -0600

    style: Next fragment to create a project was design.

--- app/src/main/java/com/listocalixto/android/mathsolar/ui/main/projects/addedit_project/fragments/AddEditProjectFragment01.kt
+++ app/src/main/java/com/listocalixto/android/mathsolar/ui/main/projects/addedit_project/fragments/AddEditProjectFragment01.kt
@@ -9,10 +9,12 @@ import android.widget.ArrayAdapter
 import android.widget.AutoCompleteTextView
 import androidx.core.view.doOnPreDraw
 import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
 import com.google.android.material.transition.MaterialSharedAxis
 import com.listocalixto.android.mathsolar.R
 import com.listocalixto.android.mathsolar.databinding.FragmentAddEditProject01Binding
 import com.listocalixto.android.mathsolar.presentation.main.projects.addedit_project.AddEditProjectViewModel
+import com.listocalixto.android.mathsolar.utils.EventObserver
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -37,6 +39,32 @@ class AddEditProjectFragment01 : Fragment(R.layout.fragment_add_edit_project_01)
             lifecycleOwner = this@AddEditProjectFragment01.viewLifecycleOwner
             addEditProjectViewModel = viewModel
 
+        }
+
+        setupNavigation()
+
+    }
+
+    private fun setupNavigation() {
+        viewModel.run {
+            nextEvent.observe(viewLifecycleOwner, EventObserver {
+                navigateToAddEditFragment02()
+            })
+        }
+    }
+
+    private fun navigateToAddEditFragment02() {
+        applyExitMotionTransition()
+        val direction = AddEditProjectFragment01Directions.actionAddEditProjectFragment01ToAddEditProjectFragment02()
+        findNavController().navigate(direction)
+    }
+
+    private fun applyExitMotionTransition() {
+        exitTransition = MaterialSharedAxis(MaterialSharedAxis.X, true).apply {
+            duration = resources.getInteger(R.integer.reply_motion_duration_large).toLong()
+        }
+        reenterTransition = MaterialSharedAxis(MaterialSharedAxis.X, false).apply {
+            duration = resources.getInteger(R.integer.reply_motion_duration_large).toLong()
         }
     }
 
